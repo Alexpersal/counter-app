60d6352352996121f8cd0177d3712b90a40c1065
Alexperezsal <al.pes79@gmail.com>
2022-03-21 19:22:14 +0100

    CounterRedux

--- src/CounterRedux.js
+++ src/CounterRedux.js
@@ -13,40 +13,48 @@ function CounterRedux() {
   const dispatchCounter = useDispatch();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>{count}</p>
-        <button
-          onClick={() =>
-            count < 10
-              ? dispatchCounter(increaseCounter(1))
-              : alert("El número no puede superar el 10")
-          }
-        >
-          Increase counter
-        </button>
-        <button
-          onClick={() =>
-            count > 0
-              ? dispatchCounter(decreaseCounter(1))
-              : alert("El número no puede bajar de 0")
-          }
-        >
-          Decrease counter
-        </button>
-        <button
-          onClick={() =>
-            count < 6
-              ? dispatchCounter(increaseCounterPlus5(5))
-              : alert("El número no puede superar el 10")
-          }
-        >
-          Increase counter +5
-        </button>
-        <button onClick={() => dispatchCounter(resetCounter())}>
-          Reset counter
-        </button>
-      </header>
+    <div className="caja">
+      <div className="Calculadora">
+        <div className="Display">{count}</div>
+        <div className="Botones">
+          <button
+            className="inc1"
+            onClick={() =>
+              count < 10
+                ? dispatchCounter(increaseCounter(1))
+                : alert("El número no puede superar el 10")
+            }
+          >
+            Incrementa 1
+          </button>
+          <button
+            className="dec1"
+            onClick={() =>
+              count > 0
+                ? dispatchCounter(decreaseCounter(1))
+                : alert("El número no puede bajar de 0")
+            }
+          >
+            Decrementa 1
+          </button>
+          <button
+            className="inc5"
+            onClick={() =>
+              count < 6
+                ? dispatchCounter(increaseCounterPlus5(5))
+                : alert("El número no puede superar el 10")
+            }
+          >
+            Incrementa 5
+          </button>
+          <button
+            className="clear"
+            onClick={() => dispatchCounter(resetCounter())}
+          >
+            Resetea Contador
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
